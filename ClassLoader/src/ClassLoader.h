4f47f81abbf53b35357500cdc204e0235cab4406
fernandoleandro1991 <fernandoleandro1991@c64d209e-cf24-80df-2a3b-490b4e205fae>
2011-12-01 21:42:47 +0000

    Agora não está funcionando.
    Começamos a montar a estrutura da JVM, colocando os .c e .h

---
+++ ClassLoader/src/ClassLoader.h
@@ -0,0 +1,12 @@
+
+/*
+ * ClassLoader.h
+ *
+ * Interface do ClassLoader.c
+ */
+#ifndef ESTRUTURAS
+#define ESTRUTURAS
+#include "Estruturas.h"
+#endif
+
+ClassFile lerClassFile(char *nomeArquivo);
