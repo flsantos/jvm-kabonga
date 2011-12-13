8eec91d19ad94c750a6030e1f8e2b66aa720560a
brunocapu <brunocapu@c64d209e-cf24-80df-2a3b-490b4e205fae>
2011-12-13 20:27:55 +0000

    Capu. funcoes de Debug da pilha

---
+++ ClassLoader/src/Debug.h
@@ -0,0 +1,16 @@
+/*
+ * Debug.h
+ *
+ *  Created on: Dec 13, 2011
+ *      Author: Julio
+ */
+
+#ifndef DEBUG_H_
+#define DEBUG_H_
+
+#include <stdio.h>
+#include "Estruturas.h"
+
+#endif /* DEBUG_H_ */
+
+void debugPrintEmpilha(char *tipo, Frame *frame);
